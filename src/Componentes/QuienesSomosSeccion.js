140f129cb68992d53127f2b34b591811ac34635a
unknown <yonyfreire@gmail.com>
2019-06-03 09:41:14 -0300

    inicio 2

--- src/Componentes/QuienesSomosSeccion.js
+++ src/Componentes/QuienesSomosSeccion.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import Slide from 'react-reveal/Slide';
+import HeadShake from 'react-reveal/HeadShake';
+import Zoom from 'react-reveal/Zoom';
+import Pulse from 'react-reveal/Pulse';
+import Fade from 'react-reveal/Fade';
+import Swing from 'react-reveal/Swing';
 import {
     Collapse,
     Navbar,
@@ -24,11 +29,11 @@ class QuienesSomosSeccion extends Component {
     render() {
         return (
             <div id="quienes">
-                <Slide animation-delay="4s" top cascade  ><h1 className="pb-4 tituloseccion">Quienes Somos</h1></Slide>
-                <img className="img-fluid" src={imgquienes}></img>
+                <Slide duration={1500} top cascade  ><h1 className="pb-4 tituloseccion">Quienes Somos</h1></Slide>
+                <Swing ><img className="img-fluid" src={imgquienes}></img></Swing >
                 <div className="mt-3">
-                <Col md="4" sm="12" className="marginauto"><img id="logoparrafo" src={logochico}></img></Col>
-                <Col md="6" sm="12" className="h6 marginauto">Somos una empresa especializada en tecnologia de la informacion.<br /> Contamos con un equipo con amplia experiencia en videovigilancia, control de Acceso, servicios informáticos, Networking, Software, Telefonía IP, entre otros. <br /> <br />Nuestro enfoque es darle a cada cliente una atención personalizada, ofreciéndole la mejor solución para sus necesidades.</Col>
+                <Zoom delay={1000}><Col md="4" sm="12" className="marginauto"><img id="logoparrafo" src={logochico}></img></Col></Zoom>
+                <Fade delay={1100} left ><Col md="6" sm="12" className="h6 marginauto">Somos una empresa especializada en tecnologia de la informacion.<br /> Contamos con un equipo con amplia experiencia en videovigilancia, control de Acceso, servicios informáticos, Networking, Software, Telefonía IP, entre otros. <br /> <br />Nuestro enfoque es darle a cada cliente una atención personalizada, ofreciéndole la mejor solución para sus necesidades.</Col></Fade>
                 </div>
             </div>
         );
--- src/Componentes/VideoSeccion.js
+++ src/Componentes/VideoSeccion.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 import YouTube from 'react-youtube';
+import HeadShake from 'react-reveal/HeadShake';
+import Zoom from 'react-reveal/Zoom';
+import Pulse from 'react-reveal/Pulse';
+import Flip from 'react-reveal/Flip';
+import RubberBand from 'react-reveal/RubberBand';
+import Slide from 'react-reveal/Slide';
+import logo from "./../media/logo/LOGOBLANCO.png"
+import logochico from "./../media/logo/logochico.png"
+import cctv from "./../media/cctvblanco.png"
+import controldeacceso from "./../media/controldeaccesoblanco.png"
+import consultoria from "./../media/consultoriablanco.png"
+import domotica from "./../media/domoticablanco.png"
 
 
 class VideoSeccion extends Component {
@@ -14,27 +26,30 @@ class VideoSeccion extends Component {
         //         rel: 0,
         //         showinfo: 0
         //       }
-            
+
         // }
 
         return (
-           
-                <div id="cabecera">
-                    {/* <div className="yt-video">
-                    <iframe src="https://www.youtube.com/embed/HktQVHNvG3s?rel=0"  allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                    </div> */}
-                {/* <div className="">
-                <YouTube
-                    videoId="HktQVHNvG3s"
-                    opts={opts}
-                    onReady={this._onReady}
-                    onEnd={this._onEnd}
-                />
-            </div> */}
-            {/* <iframe src="//www.youtube.com/embed/4t-JboICb28?rel=0;controls=0&amp;showinfo=0&amp;autoplay=1&amp;html5=1&amp;allowfullscreen=true&amp;wmode=transparent" frameborder="0" allowfullscreen id="video">
-</iframe> */}
 
-<iframe src="https://www.youtube.com/embed/HktQVHNvG3s?loop=1&amp;playlist=HktQVHNvG3s&amp;controls=0&amp;showinfo=1&amp;autoplay=1&amp;autohide=0&amp;wmode=transparent&amp;frameborder=0"  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="video"></iframe>
+            <div id="videoseccion">
+            
+             {/* LOGO Y TEXTO CENTRALIZADOS */}
+        
+        <ul class="list-unstyled" id="headerDescripcion">
+          <li >
+            <RubberBand delay={2000}><img id="logoDescripcion" src={logo}></img></RubberBand>
+          </li>
+          <Zoom duration={2000} left delay={2000}><li className="tituloDescripcion">
+            Contamos con las marcas N°1 en seguridad a nivel mundial
+        </li></Zoom>
+        <Zoom duration={2000} left delay={2000} cascade><li className="tituloDescripcion2">
+          Hikvision
+        </li></Zoom>
+        </ul>
+    
+
+
+                <iframe title="videopres" src="https://www.youtube.com/embed/1OPcUqo8VMQ?loop=1&amp;playlist=1OPcUqo8VMQ&amp;mute=1&amp;controls=0&amp;showinfo=1&amp;autoplay=1&amp;autohide=0&amp;wmode=transparent&amp;frameborder=0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="video"></iframe>
             </div>
         );
     }
